8d5b84d87c6a2e2a41c2cb3d545e687d42074c84
vladneverov <neverovprod@gmail.com>
2020-07-21 17:24:49 +0300

    init

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Категории</div>
+
+                    <div class="card-body">
+                        <select>
+                            <option value="">Выберите категорию</option>
+                            <option v-for="category in getCategories" 
+                                    :value="category.id"
+                                    :key="category.id"
+                            >
+                            {{ category.name }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.$store.dispatch('ajaxCategoriesFromDB')
+        },
+        computed: {
+            getCategories() {
+                return this.$store.getters.getCategories
+            }
+        }
+    }
+</script>
---
+++ resources/js/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        categories: []
+    },
+    getters: {
+        getCategories({ categories }) {
+            return categories
+        }
+    },
+    actions: {
+        ajaxCategoriesFromDB({ commit }) {
+            axios
+            .get("api/category")
+            .then(response => {
+                console.log(response.data.categories)
+                commit('setCategories', response.data.categories)
+            })
+            .catch(error => console.log('Ошибка!', error))
+        }
+    },
+    mutations: {
+        setCategories(state, data) {
+            return state.categories = data
+        }
+    }
+})
